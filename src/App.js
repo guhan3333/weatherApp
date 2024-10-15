f276cc60b887f8c86fa48dcd4458c1da311d115b
guhan3333 <guhanramalingam1909@gmail.com>
2024-10-15 16:20:25 +0530

    initilaze commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+
 
 function App() {
+  const [city, setCity] = useState();
+  const [CitySuggestion, setCitysuggestion] = useState([]);
+
+  const AutoCompURL = "https://api.weatherapi.com/v1/search.json?key=aa42f106a0e2473795e63351241510&q="
+
+
+
+  useEffect(() => {
+    if (city && city.length > 3) {
+      fetchAutoCompApi();
+    }
+    // fetchAutoCompApi();
+  }, [city]);
+
+
+  const fetchAutoCompApi = async () => {
+    try {
+      const response = await axios.get(AutoCompURL + city);
+      const resp = response.data;
+      // console.log("api call", resp);
+      const cityData = resp.map((data) => {
+        return `${data.name},${data.region}, ${data.country}`
+      });
+      setCitysuggestion(cityData);
+    }
+    catch (defineError) {
+      console.log('error', defineError);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container bg-secondary p-5 rounded mt-5">
+      <input type="text" className='form-control' onChange={(e) => { setCity(e.target.value) }} />
+      {/* {city && <h4>{city}</h4>} */}
+      {CitySuggestion && CitySuggestion.map((data) => {
+        return <div className='text-center bg-info rounded p-1 bg-opacity-50 border border border-opacity-50 text-white'><h4>{data}</h4></div>;
+      })}
     </div>
   );
 }
